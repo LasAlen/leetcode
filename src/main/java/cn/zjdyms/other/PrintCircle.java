d9209cbb73d8c48f08ca53601b794c839e739df8
zjdym <zjdyms@163.com>
2017-10-12 20:24:28 +0800

    添加第一个内容的解释

--- src/main/java/cn/zjdyms/other/PrintCircle.java
+++ src/main/java/cn/zjdyms/other/PrintCircle.java
@@ -2,13 +2,26 @@ package cn.zjdyms.other;
 
 import org.junit.Test;
 
+/**
+ * 1：循环打印二维数组
+ * 		解释：绕圈圈打印二维数组中的数值
+ * 		例子：{{1,2,3}，{4,5,6}，{7,8,9}} 输出:123688745
+ * 
+ * @author Allen
+ *
+ */
 public class PrintCircle {
 	@Test
 	public void test() {
-		int[][] array = {{1,2,3,4},{4,5,6,7},{7,8,9,10}};
+		int[][] array = {{1,2,3},{4,5,6},{7,8,9}};
 		printCircle1(array);
 	}
 	
+	/**
+	 * 考虑该问题，一个二维数组可以通过两个点来确定我们所需要输出的内容,即两个对角的坐标,通过四个循环打印出
+	 * 	四条边的数值即可，完成循环打印
+	 * @param array
+	 */
 	public void printCircle1(int[][] array) {
 		int i = 0;
 		int j = array.length;
